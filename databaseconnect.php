15f3d69ba64060aba43e1c4399da2708624fb201
JenniferBenedict <jbenedict1@drew.edu>
2017-11-08 15:41:37 -0500

    database connectivity
    file that allows db connectivity to all other pages

---
+++ databaseconnect.php
@@ -0,0 +1,23 @@
+<?php
+
+$user = 'root';
+$password = 'root';
+$db = 'rotc_army_supply';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysqli_init();
+$conn = mysqli_real_connect(
+   $link, 
+   $host, 
+   $user, 
+   $password, 
+   $db,
+   $port
+);
+$db_selected = mysqli_select_db(
+   $db, 
+   $link
+);
+
+?>
